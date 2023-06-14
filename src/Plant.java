caa5ed17fb3a8339505b1340591b16021449ee1c
cbgabler <carsongabler7@gmail.com>
2023-06-14 16:29:35 -0700

    new

---
+++ src/Plant.java
@@ -0,0 +1,31 @@
+import processing.core.PImage;
+
+import java.util.List;
+
+public abstract class Plant extends EntityScheduling {
+    private int health;
+    private int healthLimit;
+    public Plant(String id, Point position, List<PImage> images, double actionPeriod, double animationPeriod, int health, int healthLimit){
+        super(id, position, images, actionPeriod, animationPeriod);
+        this.health=health;
+        this.healthLimit=healthLimit;
+    }
+
+    public abstract void executeActivity(Entity entity, WorldModel world, ImageStore imageStore, EventScheduler scheduler);
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getHealthLimit() {
+        return healthLimit;
+    }
+
+    public void addHealth() {
+        this.health++;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+}
